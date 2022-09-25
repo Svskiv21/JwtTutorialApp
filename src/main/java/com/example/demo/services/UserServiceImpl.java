1f5b0af54af2afec9de44233e061c60cc27be3ec
Svskiv21 <natalia.krukar@gmail.com>
2022-09-25 12:45:27 +0200

    created domain with basic attributes, services and repositories with basic logic

---
+++ src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.services;
+
+import com.example.demo.domain.AppUser;
+import com.example.demo.domain.Role;
+import com.example.demo.repositories.RoleRepository;
+import com.example.demo.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public AppUser saveUser(AppUser user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        AppUser user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public AppUser getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<AppUser> getUsers() {
+        return userRepository.findAll();
+    }
+}
