b6d2370c319957cd7e0ca75e78f0fec3d07ed8d7
Svskiv21 <natalia.krukar@gmail.com>
2022-09-25 12:07:54 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/JwtTutorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutorialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutorialAppApplication.class, args);
+	}
+
+}
